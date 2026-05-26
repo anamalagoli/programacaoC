7bfebbafa030ad145dac0b283bd56983860c647c
anamalagoli <anacarolini.malagoli@gmail.com>
2026-05-26 18:17:22 +0000

    fimtema4

--- atividades.c
+++ atividades.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
  
-int main() {
-    int segundos;
-   
-    for (segundos = 10; segundos >= 0; segundos--) {
-        printf("%d\n", segundos);
-       
-        // Loop aninhado para simular um atraso
-        for (int i = 0; i < 100000000; i++) {
-            // Apenas para simular um atraso
-        }
+// Função recursiva para imprimir números de n até 1
+void recursiveLoop(int numero) {
+    if (numero > 0) {
+        recursiveLoop(numero - 1);  // Chama a si mesma com n - 1
+        printf("%d ", numero);  // Imprime o valor atual de n
+        
     }
+}
  
-    printf("Fogos de artifício!\n");
-   
+int main() {
+    int numero = 5;
+    printf("Contagem regressiva: ");
+    recursiveLoop(numero);
     return 0;
 }
